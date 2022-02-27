4e1a127708165c961f8da6dbcb80039e47cca70b
lgf2111 <lgf2111@gmail.com>
2022-02-27 17:25:58 +0800

    empty page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 // import { useState, useRef, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <>
+    {/* <Navbar /> */}
+    <Home />
+    </>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigation = [
+    { name: 'Home', href: '#home'},
+    { name: 'About', href: '#about'},
+    { name: 'Projects', href: '#projects'},
+    { name: 'Contact', href: '#contact'},
+  ]
   return (
-    <nav>
-    <ul>
-      <li>
-        <Link to="/home">Home</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
-  </nav>
-  )
+    <nav class="flex items-center justify-between flex-wrap bg-blue-500 p-6">
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          {navigation.map((item) => (
+          <a key={item.name} href={item.href} className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">
+            {item.name}
+          </a>
+        ))}
+
+        </div>
+      </div>
+    </nav>)
 }
